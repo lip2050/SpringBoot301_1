e6a801b45980b7743deda2d624c3af0c0826b48a
lip2050 <k.kyartel@gmail.com>
2019-10-09 12:31:41 -0700

    Model 301 assignment

---
+++ src/main/java/com/example/demo/Dog.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Dog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+    @NotNull
+    @Min(1)
+    private int age;
+
+}
